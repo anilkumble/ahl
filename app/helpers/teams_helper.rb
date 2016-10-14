55ef9c5063f6dc856012eaba53ae1de8af924bd5
Arun Kumar <arunkumarm1995@gmail.com>
2016-10-14 23:29:07 +0530

    Update content and refactor code (#7)
    * Replace cabin font with Geomanist
    
    * update content and indent code

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,18 @@
 module TeamsHelper
+  def past_position team
+    case team
+      when "Roaring Ruffianz"
+        4
+      when "Deadly Dribblerz"
+        1
+      when "Rising Warriorz"
+        2
+      when "Yo Yo Yakz"
+        6
+      when "Crouching Griffinz"
+        5
+      when "Sizzling Shooterz"
+        3
+    end
+  end
 end
